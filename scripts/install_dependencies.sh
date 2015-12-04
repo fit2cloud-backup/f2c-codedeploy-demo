11193a3cef009f2039726d827a7820f0e9f03b84
fit2cloud <fittocloud@gmail.com>
2015-12-04 16:36:42 +0800

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo install dependencies
-mkdir -p /opt/fit2cloud-demo
+if [ -d /opt/fit2cloud-demo ];then
+    echo /opt/fit2cloud-demo exists
+else
+    mkdir -p /opt/fit2cloud-demo
+fi
