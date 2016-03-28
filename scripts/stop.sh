4c4d4fec67fac4214ce824fe18eb7ca169b3b97d
fit2cloud <fittocloud@gmail.com>
2016-03-28 21:49:45 +0800

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,2 @@
 echo going to stop app
-ps aux | grep SimpleHTTPServer | grep 8089 | awk '{print "kill -9 " $2}' | bash
-echo stop app failed!
-ps aux | grep SimpleHTTPServer | grep 8080
+ps aux | grep SimpleHTTPServer | grep 8080 | awk '{print "kill -9 " $2}' | bash
