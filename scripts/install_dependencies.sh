ac7ce6dbcc9954fe32aca041c317c472fecd3f4e
liutao <tao@fit2cloud>
2016-09-13 09:41:59 +0800

    role back

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-set -e
-#
-APP_HOME=/usr/share/tomcat8-codedeploy/webapps
-WWW_DIR=$APP_HOME/app/ios
-WWW_ZIP=$APP_HOME/www.zip
 
-if [ -d $WWW_DIR ]; then
-     rm -rf $WWW_DIR
+echo install dependencies
+if [ -d /opt/fit2cloud-demo ];then
+    echo /opt/fit2cloud-demo exists
+else
+    mkdir -p /opt/fit2cloud-demo
 fi
- mkdir -p $WWW_DIR
-
-unzip  -o -d $WWW_DIR $WWW_ZIP 
-
-if [[ -f $WWW_ZIP ]]; then
-    rm $WWW_ZIP
-fi
+echo install dependencies done!
