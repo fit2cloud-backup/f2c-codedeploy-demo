4ff39ef15bf3ff4dec0c8944f3b4812e0137ec50
liutao <tao@fit2cloud>
2016-09-13 09:44:37 +0800

    test

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,3 +7,20 @@ else
     mkdir -p /opt/fit2cloud-demo
 fi
 echo install dependencies done!
+
+set -e
+#
+APP_HOME=/usr/share/tomcat8-codedeploy/webapps
+WWW_DIR=$APP_HOME/app/ios
+WWW_ZIP=$APP_HOME/www.zip
+
+if [ -d $WWW_DIR ]; then
+     rm -rf $WWW_DIR
+fi
+ mkdir -p $WWW_DIR
+
+unzip  -o -d $WWW_DIR $WWW_ZIP 
+
+if [[ -f $WWW_ZIP ]]; then
+    rm $WWW_ZIP
+fi
