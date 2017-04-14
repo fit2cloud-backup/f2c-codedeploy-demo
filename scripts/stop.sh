02271c6a1326f1f73b5029ab097a28aedba7afc1
liutao <tao@fit2cloud>
2017-04-15 01:19:21 +0800

    improve stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,12 @@
 echo going to stop app
+
 ps aux | grep SimpleHTTPServer | grep 8080 | awk '{print "kill -9 " $2}' | bash
+
+processesNum=`ps aux | grep SimpleHTTPServer | grep 8080 | grep -v grep | wc -l | sed 's/ //g'`
+if [ "$processesNum" == "0" ];then
+    echo stopped successfully!
+    exit 0
+else
+    echo stopped failed!
+    exit 1
+fi 
