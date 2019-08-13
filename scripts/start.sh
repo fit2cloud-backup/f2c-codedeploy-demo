43a8f68ed2ae9fb6c8668fb4e59315db4eb522b7
liutao <tao@fit2cloud>
2019-08-14 07:16:33 +0800

    fix first deployment failure.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,14 @@
 cd /opt/fit2cloud-demo
 echo going to start app
 nohup python -m SimpleHTTPServer 8080 > log.txt 2>&1 &
-echo app started!
-ps aux | grep SimpleHTTPServer | grep -v grep
+
+processesNum=`ps aux | grep SimpleHTTPServer | grep 8080 | grep -v grep | wc -l | sed 's/ //g'`
+ps aux | grep SimpleHTTPServer  | grep 8080 | grep -v grep
+echo process number is $processesNum
+if [ "$processesNum" == "1" ];then
+    echo app started!
+    exit 0
+else
+    echo app start failed!
+    exit 1
+fi
